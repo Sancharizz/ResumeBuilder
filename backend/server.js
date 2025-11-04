ad1c002fcf30cbb8b4e596f48659d41fe215da82
Sanchari <sanchari3011@gmail.com>
2025-11-04 17:23:27 +0530

    mods

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT ||4000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    '',
+    'https://resumexpert-sp.netlify.app/',
   ],
   credentials: true,
 }));
