608bb209570205405baeebe192c8727512dfeaf9
Sanchari <sanchari3011@gmail.com>
2025-11-04 17:02:56 +0530

    mods

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,15 @@ import resumeRoutes from './routes/resumeRoutes.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT ||4000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    '',
+  ],
+  credentials: true,
+}));
 //app.use(express.json());
 //connect db
 connectDB();
